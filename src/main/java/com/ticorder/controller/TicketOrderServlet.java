4c53d1a102f90d0cd62302f230cbb82a355c5960
Andy huang <a321489329@gmail.com>
2024-06-11 23:14:10 +0800

    更新訂票流程動態抓取購票會員

--- src/main/java/com/ticorder/controller/TicketOrderServlet.java
+++ src/main/java/com/ticorder/controller/TicketOrderServlet.java
@@ -16,6 +16,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -57,6 +58,9 @@ public class TicketOrderServlet extends HttpServlet {
         TicketTypesService ticTypesSvc = new TicketTypesService();
         CinemaService cinemaSvc = new CinemaService();
         
+
+		HttpSession session = req.getSession();
+		MemberVO member=(MemberVO) session.getAttribute("memberVO");
         
         
         //訂單複合查詢
@@ -93,10 +97,10 @@ public class TicketOrderServlet extends HttpServlet {
         
     	// 前端下拉選單動態抓取上映電影功能
  		if ("getAllShowingMovieNames".equals(action)) {
-             res.setContentType("application/json");
-             res.setCharacterEncoding("UTF-8");
              Gson gson = new Gson();
              String json = gson.toJson(ticOrderSvc.getAllShowingMovieNames());
+             res.setContentType("application/json");
+             res.setCharacterEncoding("UTF-8");
              res.getWriter().write(json);          
          }
      		
@@ -104,13 +108,12 @@ public class TicketOrderServlet extends HttpServlet {
  		if ("getShowTimeDatesByMovieId".equals(action)) {
  		    Integer movieId = Integer.parseInt(req.getParameter("movieId"));
  		    List<Date> showTimeDates = ticOrderSvc.getShowTimeDatesByMovieId(movieId);
-
+ 		    
  		    Gson gson = new Gson();
  		    String json = gson.toJson(showTimeDates);
  		    res.setContentType("application/json");
  		    res.setCharacterEncoding("UTF-8");
  		    res.getWriter().write(json);
- 		    
  		}
  		
  		// 前端下拉選單動態抓取場次功能
@@ -118,7 +121,7 @@ public class TicketOrderServlet extends HttpServlet {
  		    Integer movieId = Integer.parseInt(req.getParameter("movieId"));
  		    Date showTimeDate = Date.valueOf(req.getParameter("showTimeDate"));
  		    List<Integer> showTimes = ticOrderSvc.getShowTimesByMovieIdAndDate(movieId, showTimeDate);
- 		    // 將時間列表轉換為 JSON 格式並發送給前端
+ 		    
  		    Gson gson = new Gson();
  		    String json = gson.toJson(showTimes);
  		    res.setContentType("application/json");
@@ -131,12 +134,9 @@ public class TicketOrderServlet extends HttpServlet {
             String movieId = req.getParameter("movieId");
             String showTimeDateStr = req.getParameter("showTimeDate");
             String showTimeStr = req.getParameter("showTime");
-            
             Date showTimeDate = Date.valueOf(showTimeDateStr);
             Integer showTime = Integer.parseInt(showTimeStr);
-
-            MovieTimeVO movieTimeVO = ticOrderSvc.getShowTimeIdByMovieIdAndDateAndTime(Integer.parseInt(movieId), showTimeDate, showTime);
-            
+            MovieTimeVO movieTimeVO = ticOrderSvc.getShowTimeIdByMovieIdAndDateAndTime(Integer.parseInt(movieId), showTimeDate, showTime);            
             Integer showTimesId = movieTimeVO.getShowTimesId();
             String moviePlaybackType = null;
             switch(movieTimeVO.getMoviePlaybackType()) {
@@ -146,11 +146,10 @@ public class TicketOrderServlet extends HttpServlet {
             case 1:
             	moviePlaybackType = "3D版";
                 break;
-            };
-            
+            }; 
             JedisPool pool = JedisUtil.getJedisPool();
             Jedis jedis = pool.getResource();
-//            String sessionId = req.getSession().getId(); //會員
+            jedis.del("memberId:" + member.getMemberId());
 
             Map<String, String> member_data = new HashMap<>();
             member_data.put("movieId", movieId);
@@ -158,27 +157,22 @@ public class TicketOrderServlet extends HttpServlet {
             member_data.put("showTime", showTimeStr);
             member_data.put("showTimesId", showTimesId.toString());
             member_data.put("moviePlaybackType", moviePlaybackType);
-            jedis.hmset("memberId:" + 3, member_data); //會員
-
-            jedis.close(); // Make sure to close the Jedis instance
-
+            jedis.hmset("memberId:" + member.getMemberId(), member_data);
+            jedis.close();
+            
             res.getWriter().write("success");
         }
  		
  		//第二個頁面將會員的電影.日期.場次從Redis抓出印出在頁面
  		if ("getMemberData_Movie".equals(action)) {
- 		    // 從 Redis 中獲取存儲的數據
  		    JedisPool pool = JedisUtil.getJedisPool();
  		    Jedis jedis = pool.getResource();
-// 		    String sessionId = req.getSession().getId(); //會員
- 		    Map<String, String> member_data = jedis.hgetAll("memberId:" + 3); //會員
+ 		    Map<String, String> member_data = jedis.hgetAll("memberId:" + member.getMemberId());
  		    
  		    String showTimeDate = member_data.get("showTimeDate");
  		    String showTime = member_data.get("showTime");
  		    String moviePlaybackType = member_data.get("moviePlaybackType");
  		    String showTimesId = member_data.get("showTimesId");
-
- 		    // 使用 getMovieNameById 方法獲取電影名稱
  		    String movieId = member_data.get("movieId");
  		    String movieName = ticOrderSvc.getMovieNameById(movieId);
  		    
@@ -187,11 +181,9 @@ public class TicketOrderServlet extends HttpServlet {
  		    data.put("showTimeDate", showTimeDate);
  		    data.put("showTime", showTime);
  		    data.put("moviePlaybackType", moviePlaybackType);
- 		    data.put("showTimesId", showTimesId);
- 		    
+ 		    data.put("showTimesId", showTimesId);		    
  		    jedis.close();
  		    
- 		    // 將數據封裝成 JSON 返回給前端
  		    Gson gson = new Gson();
  		    String json = gson.toJson(data);
  		    res.setContentType("application/json");
@@ -200,25 +192,22 @@ public class TicketOrderServlet extends HttpServlet {
  		}
  		
  		if ("getAllTicketTypes".equals(action)) {
+            String json = new Gson().toJson(ticTypesSvc.getAll());
             res.setContentType("application/json");
             res.setCharacterEncoding("UTF-8");
-            String json = new Gson().toJson(ticTypesSvc.getAll());
             res.getWriter().write(json);          
         }
  		
- 		
- 		//取得
  		if ("calculateTotal".equals(action)) {
- 		// 取得票種ID和數量
  	        String[] ticketTypeIds = req.getParameterValues("ticketTypeIds");
  	        String[] quantities = req.getParameterValues("quantities");
 
- 	        // 從資料庫中取得所有票種資訊
  	        List<TicketTypesVO> ticketTypes = ticTypesSvc.getAll();
  	        Map<Integer, Integer> ticketPriceMap = new HashMap<>();
  	        for (TicketTypesVO ticketType : ticketTypes) {
  	            ticketPriceMap.put(ticketType.getTicketTypesId(), ticketType.getTicketPrice());
  	        }
+ 	        
  	        Map<Integer, String> ticketNameMap = new HashMap<>();
  	        for (TicketTypesVO ticketType : ticketTypes) {
  	    	    ticketNameMap.put(ticketType.getTicketTypesId(), ticketType.getTicketTypeName());
@@ -227,8 +216,6 @@ public class TicketOrderServlet extends HttpServlet {
  	        int totalAmount = 0;
  	        int totalQuantity = 0;
  	        List<Map<String, Object>> ticketSummary = new ArrayList<>();
-
- 	        // 計算總金額並生成票種摘要
  	        for (int i = 0; i < ticketTypeIds.length; i++) {
  	        	int ticketTypeId = Integer.parseInt(ticketTypeIds[i]);
  	        	String ticketTypeName = ticketNameMap.get(ticketTypeId);
@@ -246,25 +233,20 @@ public class TicketOrderServlet extends HttpServlet {
  	            ticketSummary.add(ticketInfo);
  	        }
 
- 	        // 準備回傳結果
  	        Map<String, Object> result = new HashMap<>();
  	        result.put("totalQuantity", totalQuantity);
  	        result.put("totalAmount", totalAmount);
  	        result.put("ticketSummary", ticketSummary);
 
- 	        // 設定回應型態為 JSON
+ 	        String json = new Gson().toJson(result);
  	        res.setContentType("application/json");
  	        res.setCharacterEncoding("UTF-8");
-
- 	        // 回傳結果
- 	        res.getWriter().write(new Gson().toJson(result));
+ 	        res.getWriter().write(json);
  	    }
  		
- 		//Redis存入第二個頁面選擇的票種及數量
  		if ("saveMemberdData_Ticket".equals(action)) {
             String ticketDataJson = req.getParameter("ticketData");
             
-         // 使用 Gson 將 JSON 字符串轉換為 Java 對象
             Gson gson = new Gson();
             List<TicketTypesVO> ticketData = gson.fromJson(ticketDataJson, new TypeToken<List<TicketTypesVO>>(){}.getType());
             
@@ -272,37 +254,33 @@ public class TicketOrderServlet extends HttpServlet {
             int totalQuantityN = 0;
             JedisPool pool = JedisUtil.getJedisPool();
             Jedis jedis = pool.getResource();
-//            String sessionId = req.getSession().getId(); //會員
+            
             Map<String, String> member_data = new HashMap<>();
-            
-            // 遍歷和處理數據
             for (TicketTypesVO TicketTypesVO : ticketData) {
-            String ticketTypeName = TicketTypesVO.getTicketTypeName();
-            int quantity = TicketTypesVO.getQuantity();
-            totalQuantityN += quantity;
-            String ticketTypeQuantity = ""+quantity;
-            
-            // 處理數據，例如存儲到數據庫
-            member_data.put("ticketTypeName"+i, ticketTypeName);
-            member_data.put("ticketTypeQuantity"+i, ticketTypeQuantity);
-            i++;
+	            String ticketTypeName = TicketTypesVO.getTicketTypeName();
+	            int quantity = TicketTypesVO.getQuantity();
+	            totalQuantityN += quantity;
+	            String ticketTypeQuantity = ""+quantity;
+	            member_data.put("ticketTypeName"+i, ticketTypeName);
+	            member_data.put("ticketTypeQuantity"+i, ticketTypeQuantity);
+	            i++;
             }
             
             String totalQuantity = ""+totalQuantityN;
             member_data.put("totalQuantity", totalQuantity);
-            jedis.hmset("memberId:" + 3, member_data); //會員
+            jedis.hmset("memberId:" + member.getMemberId(), member_data);
             jedis.close();
-
+            
             res.getWriter().write("success");
         }
  		
  		if ("get_seats".equals(action)) {
             String showTimesId = req.getParameter("showTimesId");
+            
             List<Map<String, String>> seats = cinemaSvc.getSessionSeats(showTimesId);
             
             Gson gson = new Gson();
             String json = gson.toJson(seats);
-            
             res.setContentType("application/json");
             res.setCharacterEncoding("UTF-8");
             res.getWriter().write(json);
@@ -311,8 +289,7 @@ public class TicketOrderServlet extends HttpServlet {
  		if ("get_totalQuantity".equals(action)) {
  			JedisPool pool = JedisUtil.getJedisPool();
  		    Jedis jedis = pool.getResource();
-// 		    String sessionId = req.getSession().getId();
- 		    Map<String, String> member_data = jedis.hgetAll("memberId:" + 3); //會員
+ 		    Map<String, String> member_data = jedis.hgetAll("memberId:" + member.getMemberId());
  		    String totalQuantity = member_data.get("totalQuantity");
  		    
  		    Map<String, String> data = new HashMap<>();
@@ -321,7 +298,6 @@ public class TicketOrderServlet extends HttpServlet {
             
             Gson gson = new Gson();
             String json = gson.toJson(data);
-            
             res.setContentType("application/json");
             res.setCharacterEncoding("UTF-8");
             res.getWriter().write(json);
@@ -329,37 +305,36 @@ public class TicketOrderServlet extends HttpServlet {
  		
  		if ("saveMemberdData_SeatNumber".equals(action)) {
  			String showTimesId = req.getParameter("showTimesId");
+ 			int status = Integer.parseInt(req.getParameter("status"));
  			String[] seatNumbers = req.getParameterValues("seatNumbers[]");
  			String[] seats = null;
+ 			
  			for(int i=0;i < seatNumbers.length; i++) {
  				seats = seatNumbers[i].split(",");
             }
-            int status = Integer.parseInt(req.getParameter("status"));
+ 			
+            
             for (String seatNumber : seats) {
             	cinemaSvc.updateSeatStatus(showTimesId, seatNumber, status);
             }
             
             Gson gson = new Gson();
-            String seatNumbersJson = gson.toJson(seatNumbers);
-            
+            String seatNumbersJson = gson.toJson(seatNumbers); 
             JedisPool pool = JedisUtil.getJedisPool();
             Jedis jedis = pool.getResource();
-//            String sessionId = req.getSession().getId(); //會員
 
             Map<String, String> member_data = new HashMap<>();
             member_data.put("seatNumbers", seatNumbersJson);
-            jedis.hmset("memberId:" + 3, member_data); //會員
+            jedis.hmset("memberId:" + member.getMemberId(), member_data); //會員
             jedis.close(); 
 
             res.getWriter().write("success");
         }
  		
  		if ("getMemberData_SeatNumber".equals(action)) {
- 		    // 從 Redis 中獲取存儲的數據
  		    JedisPool pool = JedisUtil.getJedisPool();
  		    Jedis jedis = pool.getResource();
-// 		    String sessionId = req.getSession().getId(); //會員
- 		    Map<String, String> member_data = jedis.hgetAll("memberId:" + 3); //會員
+ 		    Map<String, String> member_data = jedis.hgetAll("memberId:" + member.getMemberId());
  		    
  		    String seatNumbersJson = member_data.get("seatNumbers")
  		    		.replace("[", "")
@@ -373,7 +348,6 @@ public class TicketOrderServlet extends HttpServlet {
 		    data.put("seatNumbers", seatNumbers);
 		    jedis.close();
 		    
- 		    // 將數據封裝成 JSON 返回給前端
  		    Gson gson = new Gson();
  		    String json = gson.toJson(data);
  		    res.setContentType("application/json");
@@ -382,11 +356,9 @@ public class TicketOrderServlet extends HttpServlet {
  		}
  		
  		if ("getMemberData_Ticketlist".equals(action)) {
- 		    // 從 Redis 中獲取存儲的數據
  		    JedisPool pool = JedisUtil.getJedisPool();
  		    Jedis jedis = pool.getResource();
-// 		    String sessionId = req.getSession().getId(); //會員
- 		    Map<String, String> member_data = jedis.hgetAll("memberId:" + 3); //會員
+ 		    Map<String, String> member_data = jedis.hgetAll("memberId:" + member.getMemberId());
  		    
  		    int ticketTypeNameCount = 0;
             for (String key : member_data.keySet()) {
@@ -422,7 +394,7 @@ public class TicketOrderServlet extends HttpServlet {
  	        result.put("totalAmount", totalAmount);
  	        result.put("ticketSummary", ticketSummary);
  	        jedis.close();
- 		    // 將數據封裝成 JSON 返回給前端
+
  		    Gson gson = new Gson();
  		    String json = gson.toJson(result);
  		    res.setContentType("application/json");
@@ -435,15 +407,11 @@ public class TicketOrderServlet extends HttpServlet {
  			
  			JedisPool pool = JedisUtil.getJedisPool();
  		    Jedis jedis = pool.getResource();
-// 		    String sessionId = req.getSession().getId(); //會員
- 		    Map<String, String> member_data = jedis.hgetAll("memberId:" + 3); //會員
- 		    
- 		    MemberVO memberId = ticOrderSvc.findMemberById(3);
+ 		    Map<String, String> member_data = jedis.hgetAll("memberId:" + member.getMemberId()); 
+ 		    MemberVO memberId = ticOrderSvc.findMemberById(member.getMemberId());
 		    int movieOrderTotal = Integer.parseInt(movieOrderTotal_Data);
-		    Integer movieOrderId = ticOrderSvc.addOrderData(memberId, movieOrderTotal);
-//		    
+		    Integer movieOrderId = ticOrderSvc.addOrderData(memberId, movieOrderTotal);		    
 		    int movieId = Integer.parseInt(member_data.get("movieId"));
- 		    
  		    int showTimesId = Integer.parseInt(member_data.get("showTimesId"));
  		    
  		    String seatNumbersJson = member_data.get("seatNumbers")
@@ -459,13 +427,13 @@ public class TicketOrderServlet extends HttpServlet {
  	            ticketIdMap.put(ticketType.getTicketTypeName(), ticketType.getTicketTypesId());
  	        }
 		    
-		    //票名 拿票ID用
- 	       int ticketTypeNameCount = 0;
-	 	      for (String key : member_data.keySet()) {
-	              if (key.startsWith("ticketTypeName")) {
-	                  ticketTypeNameCount++;
-	              }
-	          }
+ 	        int ticketTypeNameCount = 0;
+	 	        for (String key : member_data.keySet()) {
+	                if (key.startsWith("ticketTypeName")) {
+	                    ticketTypeNameCount++;
+	                }
+	            }
+	 	      
 	 	    Transaction transaction = getSession().beginTransaction();
 
 	 	    int k = 0;
@@ -489,6 +457,8 @@ public class TicketOrderServlet extends HttpServlet {
 	        }
 	        
 	        transaction.commit();
+	        jedis.del("memberId:" + member.getMemberId());
+	        jedis.close();
             res.getWriter().write("success");
         }
      		
